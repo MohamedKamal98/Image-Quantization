c5857b411046c329a29b4f953b0f77c5cd842596
Your Name <you@example.com>
2019-12-06 16:13:48 +0200

    first template

---
+++ [TEMPLATE] ImageQuantization/ImageQuantization/QuantizationProcess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ImageQuantization
+{
+    class QuantizationProcess
+    {
+        /// <summary>
+        /// Get the 2D array of colors (size: Height x Width) from Main Form
+        /// </summary>
+        /// <returns>2D array of colors</returns>
+        public RGBPixel[,] getMatrix()
+        {
+            return MainForm.ImageMatrix;
+        }
+
+    }
+    
+        
+}
